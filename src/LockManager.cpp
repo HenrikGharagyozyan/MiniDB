ef459d389658057ad34750edefd8b349b7ce640c
HenrikGharagyozyan1 <henrik.gharagyozyan1@gmail.com>
2026-08-09 17:35:47 +0400

    feat(lock): implement LockManager with shared and exclusive lock methods

---
+++ src/LockManager.cpp
@@ -0,0 +1,47 @@
+#include "minidb/LockManager.h"
+
+namespace minidb 
+{
+
+    std::shared_mutex* LockManager::get_mutex_for_key(const std::string& key) 
+    {
+        // Acquire the global table mutex only for the duration of lookup/creation
+        std::lock_guard<std::mutex> lock(table_mutex_);
+
+        auto it = lock_table_.find(key);
+        if (it == lock_table_.end()) 
+        {
+            // If no mutex exists for this key yet, create it
+            auto [new_it, inserted] = lock_table_.emplace(
+                key, 
+                std::make_unique<std::shared_mutex>()
+            );
+            return new_it->second.get();
+        }
+
+        return it->second.get();
+    }
+
+    void LockManager::lock_shared(const std::string& key) 
+    {
+        // Get the mutex and request shared access (S-Lock)
+        get_mutex_for_key(key)->lock_shared();
+    }
+
+    void LockManager::lock_exclusive(const std::string& key) 
+    {
+        // Get the mutex and request exclusive access (X-Lock)
+        get_mutex_for_key(key)->lock();
+    }
+
+    void LockManager::unlock_shared(const std::string& key) 
+    {
+        get_mutex_for_key(key)->unlock_shared();
+    }
+
+    void LockManager::unlock_exclusive(const std::string& key) 
+    {
+        get_mutex_for_key(key)->unlock();
+    }
+
+} // namespace minidb
