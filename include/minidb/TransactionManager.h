106373917b46f82b24d3b97bb956e41e97858d45
HenrikGharagyozyan1 <henrik.gharagyozyan1@gmail.com>
2026-08-09 18:14:19 +0400

    feat(transaction): integrate LockManager into TransactionManager

--- include/minidb/TransactionManager.h
+++ include/minidb/TransactionManager.h
@@ -1,10 +1,13 @@
 #pragma once
 
 #include "minidb/Transaction.h"
+#include "minidb/LockManager.h"
+
 #include <atomic>
 #include <memory>
 #include <mutex>
 #include <unordered_map>
+
 
 namespace minidb 
 {
@@ -14,19 +17,31 @@ namespace minidb
     class TransactionManager 
     {
     public:
-        TransactionManager() = default;
+        explicit TransactionManager(LockManager* lock_manager = nullptr)
+            : lock_manager_(lock_manager) 
+        {
+        }
+
         ~TransactionManager() = default;
+
+        void set_lock_manager(LockManager* lock_manager) { lock_manager_ = lock_manager; }
 
         // Begin a new transaction
         std::shared_ptr<Transaction> begin();
 
-        // Commit (successfully complete)
+        // Commit the transaction and release locks
         void commit(Transaction* txn);
 
-        // Abort (cancel)
+        // Abort the transaction (rollback changes) and release locks
         void abort(Transaction* txn, Database* db);
 
     private:
+        // Release all locks held by the transaction
+        void release_locks(Transaction* txn);
+
+    private:
+        LockManager* lock_manager_{nullptr};
+
         // Counter for issuing unique IDs
         std::atomic<txn_id_t> next_txn_id_{0};
         
