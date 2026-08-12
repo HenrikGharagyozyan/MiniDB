880bb36c9ba4f127b1008caf2513a5e75065caec
HenrikGharagyozyan1 <henrik.gharagyozyan1@gmail.com>
2026-08-12 12:26:04 +0400

    feat: add AST structures for SQL statements including CREATE, INSERT, and SELECT

---
+++ include/minidb/sql/AST.h
@@ -0,0 +1,52 @@
+#pragma once
+#include <string>
+#include <vector>
+#include <memory>
+
+namespace minidb 
+{
+    // Base class for all SQL statements
+    struct SQLStatement 
+    {
+        virtual ~SQLStatement() = default;
+    };
+
+
+    // --- Structures for CREATE TABLE ---
+
+    // Definition of a single column (e.g. id INT)
+    struct ColumnDef 
+    {
+        std::string name;
+        std::string type; // "INT" or "VARCHAR"
+    };
+
+    // AST for a query: CREATE TABLE <name> (col1 type1, col2 type2);
+    struct CreateTableStatement : public SQLStatement 
+    {
+        std::string table_name;
+        std::vector<ColumnDef> columns;
+    };
+
+
+    // --- Structures for INSERT ---
+
+    // AST for a query: INSERT INTO <name> VALUES ('val1', 'val2');
+    struct InsertStatement : public SQLStatement 
+    {
+        std::string table_name;
+        std::vector<std::string> values;
+    };
+
+    
+    // --- Structures for SELECT ---
+
+    // AST for a query: SELECT * FROM <name>;
+    // (For now we only support selecting all columns via '*')
+    struct SelectStatement : public SQLStatement 
+    {
+        std::string table_name;
+        bool select_all = true; 
+    };
+
+} // namespace minidb
